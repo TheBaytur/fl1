c06b69fa35743a0221cae0ad9dfe1ddac99bc6f5
TheBaytur <bayturcodding@gmail.com>
2025-05-28 13:29:18 +0800

    1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Hello, World!!!',
+        
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 24,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
+}
